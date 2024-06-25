1c68df43071db880c34090c50de2bb97b023259b
sahiltambe18 <sahiltambe81922@gmail.com>
2024-06-25 23:10:10 +0530

    api

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -6,7 +6,6 @@ import { authOptions } from "@/utils/auth";
 export const GET = async ()=>{
     try {
         const data = await prisma.notices.findMany();
-        console.log(data)
         return NextResponse.json(data);
       } catch (err) {
         console.error(err);
@@ -22,25 +21,24 @@ export const POST = async (req:NextRequest)=>{
     if(!session || !session.user){
       return new NextResponse("Unauthorised Request" , {status:401})    
     }
-    let notice;
-    await req.json()
-    .then( async (data )=>{
-      console.log(data)
-      notice = data;
-    })
-    if(!notice && !notice.title && !notice.points ) return new NextResponse("Internal Server Error", {status:500}) 
-    await prisma.notices.create({data:{
-      title:notice.title,
-      points:notice.points
-    }});
-      
-      console.log("idhar")
-      return new NextResponse("Notice Added",{status:200})
-   
+    
+    const notice = await req.json();
+    if (!notice || !notice.title || !notice.points) {
+      return new NextResponse("Invalid Notice Data", { status: 400 });
+    }
 
-  } 
-  catch (error) {
-  console.log(error)
-  return new NextResponse("Internal Server Error", {status:500})
-  }
-}
+
+      await prisma.notices.create({
+        data: {
+          title: notice.title,
+          points: notice.points,
+        },
+      });
+  
+      return new NextResponse("Notice Added", { status: 200 });
+  
+    } catch (error) {
+      console.log(error);
+      return new NextResponse("Internal Server Error", { status: 500 });
+    }
+};
