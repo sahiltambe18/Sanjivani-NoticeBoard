34de90bdae690df88f8abc911a25e0ca42e75827
sahiltambe18 <sahiltambe81922@gmail.com>
2024-06-25 19:54:08 +0530

    auth-2

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react"
 import  { useRouter } from "next/navigation"
 
 
-export default function  page({origin}:{origin:string | undefined}) {
+export default function  page({origin}:{origin:string }) {
   
 
   const router = useRouter();
