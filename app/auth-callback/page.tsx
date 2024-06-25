ba586c0dc831e9fc13a28dd6b0926b931b1f6dc1
sahiltambe18 <sahiltambe81922@gmail.com>
2024-06-25 20:06:15 +0530

    callback

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,40 +1,43 @@
 'use client'
 
+import { useEffect } from "react"
 import { signIn } from "next-auth/react"
-import  { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation"
 
+interface PageProps {
+  origin: string;
+}
 
-export default function  page({origin}:{origin:string }) {
-  
-
+export default function Page({ origin }: PageProps) {
   const router = useRouter();
 
-  fetch("/auth-callback")
-  .then((data)=>{
-    if(data.status==200){
-      console.log("success")
-      router.push(origin? `/${origin}`:'/admin')
-    }
-  })
-  .catch( (err)=>{
-    console.log(err)
-  })
-  .finally( () =>{
-    console.log("err in auth log")
-  })
-  
-  signIn("credentials",{
-    redirect:true,
-    callbackUrl:`${origin ? origin : '/admin'}`
-  })
-  
-  
-  
-    
-    return (
-    <div className="p-10 flex text-3xl flex-col items-center  " >
-        <h1>Auth-Callback page</h1>
-        <p className="animate-pulse" >You will be redirected automatically. </p>
+  useEffect(() => {
+    const handleAuthCallback = async () => {
+      try {
+        const response = await fetch("/auth-callback");
+        if (response.status === 200) {
+          console.log("success");
+          router.push(origin ? `/${origin}` : '/admin');
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        console.log("err in auth log");
+      }
+    };
+
+    handleAuthCallback();
+
+    signIn("credentials", {
+      redirect: true,
+      callbackUrl: `${origin ? origin : '/admin'}`
+    });
+  }, [origin, router]);
+
+  return (
+    <div className="p-10 flex text-3xl flex-col items-center">
+      <h1>Auth-Callback page</h1>
+      <p className="animate-pulse">You will be redirected automatically.</p>
     </div>
-  )
+  );
 }
