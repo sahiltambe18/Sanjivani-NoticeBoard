1e2a748809620d96c4875b12c3228706623b779e
sahiltambe18 <sahiltambe81922@gmail.com>
2024-12-10 09:46:45 +0530

    dep list fix

--- components/DepList.tsx
+++ components/DepList.tsx
@@ -1,85 +1,168 @@
-'use client'
+'use client';
 import { typeNotice } from '@/types/notices';
-import React, { useEffect, useRef, useState } from 'react'
-
+import React, { useEffect, useRef, useState } from 'react';
 
 export default function DepList({ depId }: { depId: string }) {
   const listRef = useRef<HTMLUListElement>(null);
 
   const getData = async () => {
     let res = await fetch("/api/data", {
-      method: "GET"
+      method: "GET",
+      next: {
+        revalidate: 2000,
+      }
     });
     let dt = await res.json();
+    setData(dt.filter((d: typeNotice) => d.department===depId || d.department === 'toAll'));
+  };
 
-    setData(dt.filter((d: typeNotice) => d.department === depId || d.department === 'toAll'));
-  }
+  const getConfig = async () => {
+    const response = await fetch('/api/config', {
+      next: {
+        revalidate: 2000
+      }
+    });
+    const config = await response.json();
+    setFullScreen(config[0].fullScreen);
+  };
 
+  const [fullScreen, setFullScreen] = useState(false);
   const [data, setData] = useState<typeNotice[]>([
     {
       points: ["Please Wait..."],
-      title: "Notices Are Loading ",
+      title: "Notices Are Loading",
       createdAt: new Date(),
       id: "adcf",
     }
   ]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    getData();
-
-    const intervalId = setInterval(getData, 10000); // refetch 10sec
-
-    return () => clearInterval(intervalId);
-  }, [depId]);
+    const interval = setInterval(() => {
+      getConfig();
+      getData();
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
-    const list = listRef.current;
-    if (!list) return;
+    if (fullScreen) {
+      const currentItem = data[currentIndex];
+      const videoIndex = currentIndex
 
-    let scrollAmount = data.length > 1 ? 1 : 0;
-    let isScrollingUp = false;
-
-    const scrollInterval = setInterval(() => {
-      if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
-        isScrollingUp = true;
-        setTimeout(() => {
-          list.scrollTop = 0; // Reset to top after a brief pause for smooth transition
-          isScrollingUp = false;
-        }, 2000); // 2-second delay before resetting
-      } else {
-        if (!isScrollingUp) {
-          list.scrollTop += scrollAmount;
+      let timer: string | number | NodeJS.Timeout | undefined;
+      if (currentItem.videoUrl) {
+        const videoElement = document.getElementById(`video-${currentItem.id}`) as HTMLVideoElement;
+        if (videoElement) {
+          videoElement.addEventListener('ended', ()=> handleNext(videoIndex));
+          videoElement.play(); 
         }
+      } else {
+        timer = setTimeout(handleNext, 5000); // Adjust time for slideshow 
       }
-    }, 50); // Adjust the speed
 
-    return () => clearInterval(scrollInterval);
-  }, [data]);
+      return () => {
+        if (timer) clearTimeout(timer);
+      };
+    } else {
+      const list = listRef.current;
+      if (!list) return;
+
+      let scrollAmount = data.length > 1 ? 1 : 0;
+      let isScrollingUp = false;
+
+      const scrollInterval = setInterval(() => {
+        if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+          isScrollingUp = true;
+          setTimeout(() => {
+            list.scrollTop = 0;
+            isScrollingUp = false;
+          }, 2000);
+        } else {
+          if (!isScrollingUp) {
+            list.scrollTop += scrollAmount;
+          }
+        }
+      }, 50);
+
+      return () => clearInterval(scrollInterval);
+    }
+  }, [fullScreen, currentIndex, data]);
+
+  const handleNext = (videoIndex?:number) => {
+    setCurrentIndex((prevIndex) => {
+      let nextIndex = prevIndex + 1;
+      if(videoIndex!==undefined){
+        nextIndex = videoIndex+1
+      }
+      if (nextIndex >= data.length) {
+        // Restart the cycle
+        return 0;
+      }
+      return nextIndex;
+    });
+  };
 
   return (
-    <ul ref={listRef} className='w-full hide-scrollbar flex flex-col items-start text-[#fffef8] overflow-y-auto sm:pl-10' style={{ maxHeight: '100vh' }}>
+    <ul ref={listRef} className='w-full hide-scrollbar flex flex-col items-center text-[#fffef8] overflow-y-auto sm:pl-10' style={{ maxHeight: '100vh' }}>
       {data && data.map((notice, i) => (
-        <div className='my-4 flex flex-col gap-2' key={notice.id}>
-          {notice.title && <>
-            <h2 className='text-xl'>
-              <span className='font-light text-base'>{i + 1 + ".  "}</span>
-              {notice.title}
-            </h2>
-            <ul className='list-disc pl-8'>
-              {notice.points.length > 0 && notice.points.map((point, idx) => (
-                <li key={idx}>{point}</li>
-              ))}
-            </ul>
-          </>}
-          {notice.imageUrl && <img className='h-72 ml-2 rounded-lg border-2 border-gray-200' src={notice.imageUrl} alt={notice.title} />}
-          {notice.videoUrl &&
-            <video className='h-72 ml-2 rounded-lg border-2 border-gray-200' autoPlay loop muted >
-              <source src={notice.videoUrl} />
-            </video>}
-          {(notice.imageUrl || notice.videoUrl) && <p className='text-transparent' >-</p>}
+        <div key={notice.id} className={`my-6 flex flex-col gap-4 ${fullScreen && i !== currentIndex ? 'hidden' : ''}`}>
+          {notice.title && (
+            <>
+              <h2 className='text-3xl font-bold'>
+                <span className='font-light text-2xl'>{i + 1 + ".  "}</span>
+                {notice.title}
+              </h2>
+              <ul className='list-disc pl-8'>
+                {notice.points.length > 0 && notice.points.map((point, idx) => (
+                  <li key={idx}>{point}</li>
+                ))}
+              </ul>
+            </>
+          )}
+          {(notice.imageUrl || notice.videoUrl) && (
+            <div className={`flex justify-center items-center ${fullScreen ? 'fixed inset-0.5 z-50' : ''}`}>
+              {notice.imageUrl && (
+                <img
+                  className={`transition-all duration-500 ${fullScreen ? 'w-full h-full ' : 'w-full  max-w-3xl ml-2 rounded-lg border-2 border-gray-200 object-cover mx-auto'}`}
+                  src={notice.imageUrl}
+                  alt={notice.title}
+                />
+              )}
+              {notice.videoUrl && (
+                <video
+                  id={`video-${notice.id}`}
+                  className={`transition-all duration-500 ${fullScreen ? 'w-full h-full object-cover' : 'w-full max-w-3xl ml-2 rounded-lg border-2 border-gray-200 object-cover mx-auto'}`}
+                  autoPlay
+                  loop={!fullScreen} 
+                  muted
+                >
+                  <source src={notice.videoUrl} />
+                </video>
+              )}
+            </div>
+          )}
         </div>
       ))}
-
     </ul>
   );
 }
+
+
+
+// 'use client'
+// import { typeNotice } from '@/types/notices';
+// import React, { useEffect, useRef, useState } from 'react'
+
+
+// export default function DepList({ depId }: { depId: string }) {
+//   const listRef = useRef<HTMLUListElement>(null);
+
+//   const getData = async () => {
+//     let res = await fetch("/api/data", {
+//       method: "GET"
+//     });
+//     let dt = await res.json();
+
+//     setData(dt.filter((d: typeNotice) => d.department === depId || d.department === 'toAll'));
+//   }
