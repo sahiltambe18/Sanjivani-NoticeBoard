f4a6abccf5b946b239deb4f7885bec359759286c
Vanhere Harshal Gopinath <98075193+HarshalVanhere@users.noreply.github.com>
2025-03-16 10:57:05 +0530

    SMART DISPLAY NETWORK SYSTEM

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className='h-screen gap-10 items-center flex flex-col justify-start py-3 w-full'>
       <VideoPlayer />
       <div className='text-[#2f2b26] flex w-screen items-center justify-center h-40 font-semibold text-6xl bg-[#fbf5e7]'>
-        SANJIVANI NOTICE BOARD
+        SMART DISPLAY NETWORK SYSTEM
       </div>
 
       <div className='min-h-[40%] min-w-screen p-6 w-[95%] flex flex-col items-center gap-4 rounded-lg' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
